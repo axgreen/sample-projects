4dfbde1bb16c24916699cfeab60b7193c66e7357
axgreen <alexjgreen1@gmail.com>
2018-07-26 13:03:23 +0100

    Update gofundme_1_com.py

--- splash_crawlera_example/splash_crawlera_example/spiders/gofundme_1_com.py
+++ splash_crawlera_example/splash_crawlera_example/spiders/gofundme_1_com.py
@@ -2510,26 +2510,3 @@ class GofundmeCom_1(BasePortiaSpider):
         ).decode('utf-8')
         super(GofundmeCom_1, self).__init__(*args, **kwargs)
 
-
-
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('span small::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
-        next_page = response.css('li.next > a::attr(href)').extract_first()
-        if next_page:
-            yield SplashRequest(
-                url=response.urljoin(next_page),
-                endpoint='execute',
-                splash_headers={
-                    'Authorization': basic_auth_header(self.settings['SPLASH_APIKEY'], ''),
-                },
-                args={
-                    'lua_source': self.LUA_SOURCE,
-                    'crawlera_user': self.settings['CRAWLERA_APIKEY'],
-                },
-                cache_args=['lua_source'],
-            )
